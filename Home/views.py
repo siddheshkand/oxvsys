99199ba15f115554143b3b9f326916f51cf6d720
siddheshkand <siddkand@gmail.com>
2018-10-02 17:23:53 +0530

    landing page to launch site until 19 oct 2018

--- Home/views.py
+++ Home/views.py
@@ -1,6 +1,24 @@
+import datetime
+
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'Home/home.html')
+    messages = ""
+    date = datetime.datetime(year=2018, month=10, day=19) - datetime.datetime.now()
+    if request.method == 'POST':
+        messages = "We will contact you soon."
+
+    days = date.days
+    hours = date.seconds // 3600
+    minutes = (date.seconds // 60) % 60
+    print(days)
+    print(hours)
+    print(minutes)
+    return render(request, 'Home/index.html', {
+        'messages': messages,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+    })
--- oxvsys/settings.py
+++ oxvsys/settings.py
@@ -50,11 +50,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'oxvsys.urls'
 
+# noinspection PyUnresolvedReferences
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
