53f510ae937f826d30f71e3d9859c0127b4971be
siddheshkand <siddkand@gmail.com>
2019-01-02 09:02:44 +0530

    v2

--- oxvsys/settings.py
+++ oxvsys/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'bzi%4j$%=2!c9db!wz6@evj@@)+8810*#qr(%=qy#fp!p%uczt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['oxvsys.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['oxvsys.pythonanywhere.com', '127.0.0.1', '*']
 
 # Application definition
 
