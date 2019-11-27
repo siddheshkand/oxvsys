8282876be0c1975418d85d1ee1046ab6bc643b72
siddkand <siddkand@gmail.com>
2019-11-27 22:35:00 +0530

    ox v 2.3

--- oxvsys/settings.py
+++ oxvsys/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bzi%4j$%=2!c9db!wz6@evj@@)+8810*#qr(%=qy#fp!p%uczt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['oxvsys.pythonanywhere.com', '127.0.0.1', '*']
 
